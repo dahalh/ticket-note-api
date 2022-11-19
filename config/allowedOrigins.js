c775d591f2959ea1035581a3907733955b6af2b2
dahalh <dahalh@outlook.com>
2022-11-19 15:34:15 +1100

    ready for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-  // "http://localhost:3000",
-  "https://htechnotes.onrender.com",
-];
+    'https://technotes.onrender.com'
+]
 
-module.exports = allowedOrigins;
+module.exports = allowedOrigins
